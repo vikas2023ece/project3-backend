05f3688e6b67020e137c4495692902a1de713135
VIKAS S S <vikas.ss2023ece@sece.ac.in>
2025-03-12 20:20:44 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.set('trust proxy', true); 
-
 app.use(helmet()); 
 app.use(bodyParser.json());
-//app.use(cors()); 
+app.use(cors()); 
 
-app.use(cors({
-  origin: ["http://localhost:3000", "https://atten-tracker.netlify.app"], 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
